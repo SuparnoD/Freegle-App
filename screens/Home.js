c915fae3f4a1ef0715da73c05bc77175d0f2818e
SuparnoD <suparnodeb@hotmail.co.uk>
2022-03-25 18:47:35 +0000

    home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,54 +1,44 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
-import { Feather } from "@expo/vector-icons";
 
 import { Colors } from "../constants/Colors";
 
 const Home = () => {
   return (
     <View style={styles.container}>
-        <View style={styles.topContainer}>
-      <Image style={styles.logo} source={require("../assets/logo.png")} />
-      <Text style={styles.heading}>
-        Don't throw it away,{"\n"}give it away!
-      </Text>
+      <View style={styles.topContainer}>
+        <Image style={styles.logo} source={require("../assets/logo.png")} />
+        <Text style={styles.heading}>
+          DON'T THROW IT AWAY,{"\n"}GIVE IT AWAY!
+        </Text>
+      </View>
+      <View style={{marginBottom: 10, bottom: 20}}>
+        <Text>What would you like to do?</Text>
       </View>
 
-      <Text>What would you like to do?</Text>
-
-      <View style={{marginBottom: 0}}>
-
+      <View style={{ bottom: 20}}>
         <TouchableOpacity style={styles.button}>
           <View style={styles.buttonStyle}>
-            <View style={{ right: 10 }}>
-              <AntDesign name="message1" size={24} color="white" />
-            </View>
-            <Text style={{ color: "white" }}>Message</Text>
+            <Text style={{ color: "white" }}>POST</Text>
           </View>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button2}>
           <View style={styles.buttonStyle}>
-            <View style={{ right: 25 }}>
-              <Feather name="map-pin" size={28} color="white" />
-            </View>
-            <Text style={{ color: "white" }}>Map</Text>
+            <Text style={{ color: Colors.primary }}>BROWSE</Text>
           </View>
         </TouchableOpacity>
-
       </View>
 
-      <View style={{position: 'absolute', bottom: 15,}}>
-      <View style={styles.logoContainer}>
-      <Image source={require("../assets/bytemark.png")} />
-      <Image source={require("../assets/mythic-beasts.png")} />
-      </View>
+      <View style={{ position: "absolute", bottom: 15, paddingTop: 100 }}>
+        <View style={styles.logoContainer}>
+          <Image source={require("../assets/bytemark.png")} />
+          <Image source={require("../assets/mythic-beasts.png")} />
+        </View>
 
-      <Text style={{ textAlign: "center", fontSize: 12 }}>
-        Freegle is registered as a charity with HMRC (ref. XT32865) {"\n"}{" "}
-        Kindly supported by Bytemark & Mythic Beasts
-      </Text>
+        <Text style={{ textAlign: "center", fontSize: 12 }}>
+          Freegle is registered as a charity with HMRC (ref. XT32865) {"\n"}{" "}
+          Kindly supported by Bytemark & Mythic Beasts
+        </Text>
       </View>
     </View>
   );
@@ -62,7 +52,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   topContainer: {
-      bottom: 70,
+    bottom: 70,
   },
   heading: {
     textAlign: "center",
@@ -74,40 +64,56 @@ const styles = StyleSheet.create({
   button: {
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 15,
+    borderRadius: 25,
     borderWidth: 1,
-    borderColor: "white",
+    borderColor: Colors.primary,
     backgroundColor: Colors.primary,
-    width: 130,
+    width: 250,
     height: 58,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 1,
     shadowRadius: 1,
     elevation: 5,
-    margin: 3.5,
+    margin: 12,
+  },
+  button2: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 25,
+    borderWidth: 1.5,
+    borderColor: Colors.primary,
+    backgroundColor: "white",
+    width: 250,
+    height: 58,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 1,
+    shadowRadius: 1,
+    elevation: 5,
+    margin: 12,
   },
   buttonStyle: {
     flexDirection: "row",
     alignItems: "center",
   },
   logoContainer: {
-      flexDirection: "row",
-      justifyContent: "space-around",
-      alignItems: "center",
-      width: 350,
-  },
-  bottomContainer:{
     flexDirection: "row",
-    position: 'absolute',
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: 350,
+  },
+  bottomContainer: {
+    flexDirection: "row",
+    position: "absolute",
     bottom: 5,
     justifyContent: "space-around",
     width: 350,
   },
   bottomText: {
-      fontSize: 12,
-      textAlign: "center"
-  }
+    fontSize: 12,
+    textAlign: "center",
+  },
 });
 
 export default Home;
