c2e2969bc8b4e29214f67839b68e0d90656b3f31
SuparnoD <suparnodeb@hotmail.co.uk>
2022-03-30 16:17:05 +0100

    post items are fetched from server

--- util/http.js
+++ util/http.js
@@ -1,7 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'https://freegle-server-default-rtdb.europe-west1.firebasedatabase.app';
+const url =
+  "https://freegle-server-default-rtdb.europe-west1.firebasedatabase.app";
 
 export function storePost(postData) {
-    axios.post((url+'/post.json'), postData);
+  axios.post(url + "/post.json", postData);
+}
+
+export async function fetchPost() {
+  const response = await axios.get(url + "/post.json");
+
+  const posts = [];
+
+  for (const key in response.data) {
+    const postObj = {
+      id: key,
+      description: response.data[key].description,
+      quantity: response.data[key].quantity,
+      title: response.data[key].title,
+      type: response.data[key].type,
+      user: response.data[key].user,
+    };
+    posts.push(postObj);
+  }
+  return posts;
 }
