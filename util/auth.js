151ea5445a7280ffb66fbcf9a464ccc3a21c7a6b
Suparna Deb <suparnodeb@hotmail.co.uk>
2022-10-24 00:25:33 +0100

    Update auth.js

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyCiJj8Z7oOC6gUVT7Oz0v9oiWEEWoADsB8";
+const API_KEY = "";
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
